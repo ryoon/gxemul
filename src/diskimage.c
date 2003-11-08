f34a2754110c52a692f1aecd2455f89c2eb6bc76
debug <debug@dbbdf1f2-f3a0-11dc-8807-f0e41a653009>
2003-11-08 09:03:07 +0000

    Initial version.
    git-svn-id: file:///home/ryo_on/repos/gxemul-repo/gxemul-legacy/trunk@201 dbbdf1f2-f3a0-11dc-8807-f0e41a653009

---
+++ src/diskimage.c
@@ -0,0 +1,168 @@
+/*
+ *  Copyright (C) 2003 by Anders Gavare.  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *  2. Redistributions in binary form must reproduce the above copyright  
+ *     notice, this list of conditions and the following disclaimer in the 
+ *     documentation and/or other materials provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ *  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ *  ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE   
+ *  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ *  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ *  OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ *  HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ *  OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ *  SUCH DAMAGE.
+ *
+ *
+ *  $Id: diskimage.c,v 1.1 2003-11-08 09:03:07 debug Exp $
+ *
+ *  Disk image support.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+#include "misc.h"
+#include "diskimage.h"
+
+
+struct diskimage {
+	char		*fname;
+	off_t		total_size;
+	int		writable;
+
+	FILE		*f;
+};
+
+
+#define	MAX_DISKIMAGES		8
+
+static struct diskimage *diskimages[MAX_DISKIMAGES];
+static int n_diskimages = 0;
+
+
+/*
+ *  diskimage_access():
+ *
+ *  Read from or write to a disk image.
+ *
+ *  Returns 1 if the access completed successfully, 0 otherwise.
+ */
+int diskimage_access(int disk_id, int writeflag, off_t offset, unsigned char *buf, size_t len)
+{
+	int len_done;
+
+	if (disk_id >= n_diskimages || diskimages[disk_id]==NULL) {
+		fatal("trying to access a non-existant disk image (%i)\n", disk_id);
+		exit(1);
+	}
+
+	fseek(diskimages[disk_id]->f, offset, SEEK_SET);
+
+	if (writeflag)
+		len_done = fwrite(buf, 1, len, diskimages[disk_id]->f);
+	else {
+		len_done = fread(buf, 1, len, diskimages[disk_id]->f);
+		if (len_done < len)
+			memset(buf + len_done, 0, len-len_done);
+	}
+
+	/*  Warn about non-complete data transfers:  */
+	if (len_done != len) {
+		fatal("diskimage_access(): disk_id %i, transfer not completed. len=%i, len_done=%i\n",
+		    disk_id, len, len_done);
+		return 0;
+	}
+
+	return 1;
+}
+
+
+/*
+ *  diskimage_add():
+ *
+ *  Add a disk image.
+ *
+ *  Returns an integer >= 0 identifying the disk image.
+ */
+int diskimage_add(char *fname)
+{
+	int id;
+	FILE *f;
+
+	if (n_diskimages >= MAX_DISKIMAGES) {
+		fprintf(stderr, "too many disk images\n");
+		exit(1);
+	}
+
+	id = n_diskimages;
+
+	diskimages[id] = malloc(sizeof(struct diskimage));
+	if (diskimages[id] == NULL) {
+		fprintf(stderr, "out of memory\n");
+		exit(1);
+	}
+	memset(diskimages[id], 0, sizeof(struct diskimage));
+	diskimages[id]->fname = malloc(strlen(fname) + 1);
+	if (diskimages[id]->fname == NULL) {
+		fprintf(stderr, "out of memory\n");
+		exit(1);
+	}
+	strcpy(diskimages[id]->fname, fname);
+
+	/*  Measure total_size:  */
+	f = fopen(fname, "r");
+	if (f == NULL) {
+		perror(fname);
+		exit(1);
+	}
+	fseek(f, 0, SEEK_END);
+	diskimages[id]->total_size = ftell(f);
+	fclose(f);
+
+	diskimages[id]->writable = access(fname, W_OK) == 0? 1 : 0;
+
+	diskimages[id]->f = fopen(fname, diskimages[id]->writable? "r+" : "r");
+	if (diskimages[id]->f == NULL) {
+		perror(fname);
+		exit(1);
+	}
+
+	n_diskimages ++;
+
+	return id;
+}
+
+
+/*
+ *  diskimage_dump_info():
+ *
+ *  Debug dump of all diskimages that are loaded.
+ *
+ *  TODO:  The word 'adding' isn't really correct, as all diskimages
+ *         are actually already added when this function is called.
+ */
+void diskimage_dump_info(void)
+{
+	int i;
+
+	for (i=0; i<n_diskimages; i++) {
+		debug("adding diskimage %i: '%s', %s, %li bytes\n",
+		    i, diskimages[i]->fname,
+		    diskimages[i]->writable? "read/write" : "read-only",
+		    (long int) diskimages[i]->total_size);
+	}
+}
+

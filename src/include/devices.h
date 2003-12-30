56cafac18addeee14d73398d48095a59f0df838d
debug <debug@dbbdf1f2-f3a0-11dc-8807-f0e41a653009>
2003-12-30 03:06:55 +0000

    Adding rd94.
    git-svn-id: file:///home/ryo_on/repos/gxemul-repo/gxemul-legacy/trunk@313 dbbdf1f2-f3a0-11dc-8807-f0e41a653009

--- src/include/devices.h
+++ src/include/devices.h
@@ -26,7 +26,7 @@
  *  SUCH DAMAGE.
  *
  *
- *  $Id: devices.h,v 1.8 2003-12-29 00:52:44 debug Exp $
+ *  $Id: devices.h,v 1.9 2003-12-30 03:06:55 debug Exp $
  *
  *  Memory mapped devices:
  */
@@ -210,6 +210,11 @@ void dev_ps2_gif_init(struct memory *mem, uint64_t baseaddr);
 #define	DEV_PS2_GS_LENGTH		0x0000000000002000
 int dev_ps2_gs_access(struct cpu *cpu, struct memory *mem, uint64_t relative_addr, unsigned char *data, size_t len, int writeflag, void *);
 void dev_ps2_gs_init(struct memory *mem, uint64_t baseaddr);
+
+/*  dev_rd94.c:  */
+#define	DEV_RD94_LENGTH			0x1000
+int dev_rd94_access(struct cpu *cpu, struct memory *mem, uint64_t relative_addr, unsigned char *data, size_t len, int writeflag, void *);
+void dev_rd94_init(struct cpu *cpu, struct memory *mem, uint64_t baseaddr);
 
 /*  dev_sii.c:  */
 #define	DEV_SII_LENGTH			0x0000000000000100

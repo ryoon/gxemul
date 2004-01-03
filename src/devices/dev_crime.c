44bd380957703d75aa3cb7c70b79f0b3c51f7f20
debug <debug@dbbdf1f2-f3a0-11dc-8807-f0e41a653009>
2004-01-03 03:11:53 +0000

    2003 => 2003-2004
    git-svn-id: file:///home/ryo_on/repos/gxemul-repo/gxemul-legacy/trunk@347 dbbdf1f2-f3a0-11dc-8807-f0e41a653009

--- src/devices/dev_crime.c
+++ src/devices/dev_crime.c
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2003 by Anders Gavare.  All rights reserved.
+ *  Copyright (C) 2003-2004 by Anders Gavare.  All rights reserved.
  *
  *  Redistribution and use in source and binary forms, with or without
  *  modification, are permitted provided that the following conditions are met:
@@ -23,7 +23,7 @@
  *  SUCH DAMAGE.
  *   
  *
- *  $Id: dev_crime.c,v 1.4 2004-01-02 22:18:34 debug Exp $
+ *  $Id: dev_crime.c,v 1.5 2004-01-03 03:11:53 debug Exp $
  *  
  *  SGI "crime".
  */

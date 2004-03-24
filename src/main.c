4a2d52c926c59790144557261430f25c70471642
debug <debug@dbbdf1f2-f3a0-11dc-8807-f0e41a653009>
2004-03-24 02:45:02 +0000

    SGI-IP25 is R10000.
    git-svn-id: file:///home/ryo_on/repos/gxemul-repo/gxemul-legacy/trunk@761 dbbdf1f2-f3a0-11dc-8807-f0e41a653009

--- src/main.c
+++ src/main.c
@@ -23,7 +23,7 @@
  *  SUCH DAMAGE.
  *
  *
- *  $Id: main.c,v 1.26 2004-03-23 02:30:56 debug Exp $
+ *  $Id: main.c,v 1.27 2004-03-24 02:45:02 debug Exp $
  *
  *  TODO:  Move out stuff into structures, separating things from main()
  *         completely.
@@ -359,7 +359,7 @@ int get_cmd_args(int argc, char *argv[])
 	if (emulation_type == EMULTYPE_ARC && !emul_cpu_name[0])
 		strcpy(emul_cpu_name, "R4000");
 
-	if (emulation_type == EMULTYPE_SGI && machine == 27 && !emul_cpu_name[0])
+	if (emulation_type == EMULTYPE_SGI && (machine == 25 || machine == 27) && !emul_cpu_name[0])
 		strcpy(emul_cpu_name, "R10000");
 
 	if (emulation_type == EMULTYPE_SGI && !emul_cpu_name[0])

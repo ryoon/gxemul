6fa54f73832ee8e16adb07bd37d756d86fe8aa60
debug <debug@dbbdf1f2-f3a0-11dc-8807-f0e41a653009>
2004-06-18 12:19:42 +0000

    Adding R14000. TODO: lowest 8 bits of PrID? Coprocessor ID (src/coproc.c)?
    git-svn-id: file:///home/ryo_on/repos/gxemul-repo/gxemul-legacy/trunk@1031 dbbdf1f2-f3a0-11dc-8807-f0e41a653009

--- src/include/misc.h
+++ src/include/misc.h
@@ -26,7 +26,7 @@
  *  SUCH DAMAGE.
  *
  *
- *  $Id: misc.h,v 1.58 2004-06-17 22:52:27 debug Exp $
+ *  $Id: misc.h,v 1.59 2004-06-18 12:19:42 debug Exp $
  *
  *  Misc. definitions for mips64emul.
  *
@@ -178,6 +178,7 @@ struct cpu_type_def {
 	{ "R4700",	MIPS_R4700, 0x00,	0,	EXC4K, MMU4K,	3,	48 }, /*  No DCOUNT?  */ \
 	{ "R8000",	MIPS_R8000, 0,		0,	EXC4K, MMU8K,	4,     192 }, /*  192 tlb entries? or 384?  */ \
 	{ "R12000",	MIPS_R12000,0x23,	0,	EXC4K, MMU10K,	4,	64 }, \
+	{ "R14000",	MIPS_R14000,0,		0,	EXC4K, MMU10K,	4,	64 }, \
 	{ "R5000",	MIPS_R5000, 0x21,	DCOUNT,	EXC4K, MMU4K,	4,	48 }, \
 	{ "R5900",	MIPS_R5900, 0x20,	0,	EXC4K, MMU4K,	3,	48 }, \
 	{ "VR5432",	MIPS_R5400, 13,		0,	EXC4K, MMU4K,	-1,	-1 }, /*  DCOUNT?  */ \

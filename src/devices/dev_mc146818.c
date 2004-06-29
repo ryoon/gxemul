c1f3f94cfa9e10549d09362045c9d94d9fda0cc1
debug <debug@dbbdf1f2-f3a0-11dc-8807-f0e41a653009>
2004-06-29 02:34:48 +0000

    Update.
    git-svn-id: file:///home/ryo_on/repos/gxemul-repo/gxemul-legacy/trunk@1170 dbbdf1f2-f3a0-11dc-8807-f0e41a653009

--- src/devices/dev_mc146818.c
+++ src/devices/dev_mc146818.c
@@ -23,7 +23,7 @@
  *  SUCH DAMAGE.
  *   
  *
- *  $Id: dev_mc146818.c,v 1.23 2004-06-29 02:30:34 debug Exp $
+ *  $Id: dev_mc146818.c,v 1.24 2004-06-29 02:34:48 debug Exp $
  *  
  *  MC146818 real-time clock, used by many different machines types.
  *
@@ -51,7 +51,7 @@ extern struct cpu **cpus;
 
 #define	to_bcd(x)	( (x/10) * 16 + (x%10) )
 
-#define MC146818_DEBUG
+/* #define MC146818_DEBUG */
 #define	TICK_STEPS_SHIFT	10
 
 

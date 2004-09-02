858af8862c320506926473a0c2bb8c7a9d69382a
debug <debug@dbbdf1f2-f3a0-11dc-8807-f0e41a653009>
2004-09-02 00:29:28 +0000

    Trying to separate out some stuff from main.c.
    git-svn-id: file:///home/ryo_on/repos/gxemul-repo/gxemul-legacy/trunk@1570 dbbdf1f2-f3a0-11dc-8807-f0e41a653009

---
+++ src/include/emul.h
@@ -0,0 +1,85 @@
+#ifndef	EMUL_H
+#define	EMUL_H
+
+/*
+ *  Copyright (C) 2004 by Anders Gavare.  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *  2. Redistributions in binary form must reproduce the above copyright  
+ *     notice, this list of conditions and the following disclaimer in the 
+ *     documentation and/or other materials provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ *  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ *  ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE   
+ *  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ *  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ *  OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ *  HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ *  OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ *  SUCH DAMAGE.
+ *
+ *
+ *  $Id: emul.h,v 1.1 2004-09-02 00:29:28 debug Exp $
+ */
+
+#include "misc.h"
+
+struct emul {
+	char	emul_cpu_name[50];
+	int	emulation_type;
+	int	machine;
+	char	*machine_name;
+
+	int	random_mem_contents;
+	int	physical_ram_in_mb;
+	int	booting_from_diskimage;
+
+	int	quiet_mode;
+	int	show_opcode_statistics;
+	int	prom_emulation;
+	int	register_dump;
+
+	/*  PC Dumppoints: if the PC value ever matches one of these, we set
+		register_dump = instruction_trace = 1  */
+	int	n_dumppoints;
+	char	*dumppoint_string[MAX_PC_DUMPPOINTS];
+	uint64_t dumppoint_pc[MAX_PC_DUMPPOINTS];
+	int	dumppoint_flag_r[MAX_PC_DUMPPOINTS];
+	    /*  0 for instruction trace, 1 for instr.trace + register dump  */
+
+	int	bintrans_enable;
+	int	instruction_trace;
+	int	single_step;
+	int	trace_on_bad_address;
+	int	show_nr_of_instructions;
+	int64_t	max_instructions;
+	int	emulated_hz;
+	int	max_random_cycles_per_chunk;
+	int	speed_tricks;
+	int	userland_emul;
+	char	*boot_kernel_filename;
+	char	*boot_string_argument;
+
+	int	bootstrap_cpu;
+	int	use_random_bootstrap_cpu;
+	int	ncpus;
+	struct cpu **cpus;
+
+	int	automatic_clock_adjustment;
+
+	int	show_trace_tree;
+	int	tlb_dump;
+	int	verbose;
+	int	use_x11;
+	int	x11_scaledown;
+};
+
+
+#endif	/*  EMUL_H  */

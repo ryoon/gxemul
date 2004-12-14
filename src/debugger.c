5a27ab2c18fdd26a3b147c01994a2869eb43bb74
debug <debug@dbbdf1f2-f3a0-11dc-8807-f0e41a653009>
2004-12-14 03:29:26 +0000

    Update.
    git-svn-id: file:///home/ryo_on/repos/gxemul-repo/gxemul-legacy/trunk@2029 dbbdf1f2-f3a0-11dc-8807-f0e41a653009

--- src/debugger.c
+++ src/debugger.c
@@ -23,7 +23,7 @@
  *  SUCH DAMAGE.
  *
  *
- *  $Id: debugger.c,v 1.2 2004-12-14 03:09:40 debug Exp $
+ *  $Id: debugger.c,v 1.3 2004-12-14 03:29:26 debug Exp $
  *
  *  Single-step debugger.
  */
@@ -72,6 +72,9 @@ static int exit_debugger;
 static char last_cmd[MAX_CMD_LEN];
 static int last_cmd_len = 0;
 
+static uint64_t last_dump_addr = 0xffffffff80000000ULL;
+static uint64_t last_unasm_addr = 0xffffffff80000000ULL;
+
 
 /*
  *  debugger_activate():
@@ -117,6 +120,77 @@ static void debugger_cmd_continue(struct emul *emul, char *cmd_line)
 }
 
 
+/*
+ *  debugger_cmd_dump():
+ */
+static void debugger_cmd_dump(struct emul *emul, char *cmd_line)
+{
+	uint64_t addr, addr_start, addr_end;
+	struct cpu *c;
+	struct memory *m;
+	int x, r;
+
+	if (cmd_line[0] != '\0')
+		last_dump_addr = strtoll(cmd_line + 1, NULL, 16);
+
+	addr_start = last_dump_addr;
+	addr_end = addr_start + 256;
+
+	if (emul->cpus == NULL) {
+		printf("No cpus (?)\n");
+		return;
+	}
+	c = emul->cpus[emul->bootstrap_cpu];
+	if (c == NULL) {
+		printf("emul->cpus[emul->bootstrap_cpu] = NULL\n");
+		return;
+	}
+	m = emul->cpus[emul->bootstrap_cpu]->mem;
+
+	addr = addr_start & ~0xf;
+
+	while (addr < addr_end) {
+		unsigned char buf[16];
+		memset(buf, 0, sizeof(buf));
+		r = memory_rw(c, m, addr, &buf[0], sizeof(buf), MEM_READ,
+		    CACHE_NONE | NO_EXCEPTIONS);
+
+		printf("0x%016llx  ", (long long)addr);
+
+		if (r == MEMORY_ACCESS_FAILED)
+			printf("(memory access failed)\n");
+		else {
+			for (x=0; x<16; x++) {
+				if (addr + x >= addr_start &&
+				    addr + x < addr_end)
+					printf("%02x%s", buf[x],
+					    (x&3)==3? " " : "");
+				else
+					printf("  %s", (x&3)==3? " " : "");
+			}
+			printf(" ");
+			for (x=0; x<16; x++) {
+				if (addr + x >= addr_start &&
+				    addr + x < addr_end)
+					printf("%c", (buf[x]>=' ' &&
+					    buf[x]<127)? buf[x] : '.');
+				else
+					printf(" ");
+			}
+			printf("\n");
+		}
+
+		addr += sizeof(buf);
+	}
+
+	last_dump_addr = addr_end;
+
+	/*  Repetition of this command should be done with no args:  */
+	last_cmd_len = 4;
+	strcpy(last_cmd, "dump");
+}
+
+
 /*  This is defined below.  */
 static void debugger_cmd_help(struct emul *emul, char *cmd_line);
 
@@ -133,6 +207,30 @@ static void debugger_cmd_quit(struct emul *emul, char *cmd_line)
 	emul->exit_without_entering_debugger = 1;
 	emul->single_step = 0;
 	exit_debugger = 1;
+}
+
+
+/*
+ *  debugger_cmd_registers():
+ */
+static void debugger_cmd_registers(struct emul *emul, char *cmd_line)
+{
+	int i;
+
+	for (i=0; i<emul->ncpus; i++)
+		cpu_register_dump(emul->cpus[i]);
+
+	last_cmd_len = 0;
+}
+
+
+/*
+ *  debugger_cmd_step():
+ */
+static void debugger_cmd_step(struct emul *emul, char *cmd_line)
+{
+	/*  Special hack, see debugger() for more info.  */
+	exit_debugger = -1;
 }
 
 
@@ -162,20 +260,26 @@ static struct cmd cmds[] = {
 	"continue", "",	0, debugger_cmd_continue,
 		"continue execution",
 
-	"dump", "[addr]", 0, NULL/*TODO*/,
-		"dumps memory contents in hex and ASCII",
+	"dump", "[addr]", 0, debugger_cmd_dump,
+		"dump memory contents in hex and ASCII",
 
 	"help", "", 0, debugger_cmd_help,
-		"prints this help message",
+		"print this help message",
 
 	"quit",	"", 0, debugger_cmd_quit,
-		"quits the emulator",
+		"quit the emulator",
 
-	"quiet", "", 0, NULL,
-		"toggles quiet_mode on or off",
+	"quiet", "", 0, NULL /*  TODO  */,
+		"toggle quiet_mode on or off",
+
+	"registers", "", 0, debugger_cmd_registers,
+		"dumps all CPUs' register values",
+
+	"step", "", 0, debugger_cmd_step,
+		"single step one instruction",
 
 	"version", "", 0, debugger_cmd_version,
-		"prints version information",
+		"print version information",
 
 	NULL, NULL, 0, NULL, NULL
 };
@@ -183,6 +287,8 @@ static struct cmd cmds[] = {
 
 /*
  *  debugger_cmd_help():
+ *
+ *  Print a list of available commands.
  *
  *  NOTE: This is placed after the cmds[] array, because it needs to
  *  access it.
@@ -225,54 +331,6 @@ static void debugger_cmd_help(struct emul *emul, char *cmd_line)
 
 
 /****************************************************************************/
-
-
-/*
- *  debugger_dump():
- *
- *  Dump emulated memory in hex and ASCII.
- */
-static void debugger_dump(struct emul *emul, uint64_t addr, int lines)
-{
-	struct cpu *c;
-	struct memory *m;
-	int x, r;
-
-	if (emul->cpus == NULL) {
-		printf("No cpus (?)\n");
-		return;
-	}
-	c = emul->cpus[emul->bootstrap_cpu];
-	if (c == NULL) {
-		printf("emul->cpus[emul->bootstrap_cpu] = NULL\n");
-		return;
-	}
-	m = emul->cpus[emul->bootstrap_cpu]->mem;
-
-	while (lines -- > 0) {
-		unsigned char buf[16];
-		memset(buf, 0, sizeof(buf));
-		r = memory_rw(c, m, addr, &buf[0], sizeof(buf), MEM_READ,
-		    CACHE_NONE | NO_EXCEPTIONS);
-
-		printf("0x%016llx  ", (long long)addr);
-
-		if (r == MEMORY_ACCESS_FAILED)
-			printf("(memory access failed)\n");
-		else {
-			for (x=0; x<16; x++)
-				printf("%02x%s", buf[x],
-				    (x&3)==3? " " : "");
-			printf(" ");
-			for (x=0; x<16; x++)
-				printf("%c", (buf[x]>=' ' && buf[x]<127)?
-				    buf[x] : '.');
-			printf("\n");
-		}
-
-		addr += sizeof(buf);
-	}
-}
 
 
 /*
@@ -370,6 +428,8 @@ static void debugger_tlbdump(struct emul *emul)
  *  debugger_readline():
  *
  *  Read a line from the terminal.
+ *
+ *  TODO: Cursor keys for line editing?
  */
 static void debugger_readline(char *cmd, int max_cmd_len, int *cmd_len)
 {
@@ -416,8 +476,6 @@ void debugger(void)
 {
 	int i, n, i_match, cmd_len, matchlen;
 	char cmd[MAX_CMD_LEN];
-	static uint64_t last_dump_addr = 0xffffffff80000000ULL;
-	static uint64_t last_unasm_addr = 0xffffffff80000000ULL;
 
 	exit_debugger = 0;
 
@@ -498,41 +556,18 @@ void debugger(void)
 			printf("FATAL ERROR: internal error in debugger.c:"
 			    " no handler for this command?\n");
 
-#if 0
-		} else if (strcasecmp(cmd, "d") == 0 ||
-		    strcasecmp(cmd, "dump") == 0) {
-			debugger_dump(debugger_emul, last_dump_addr, 8);
-			last_dump_addr += 8*16;
-		} else if (strncasecmp(cmd, "d ", 2) == 0 ||
-		    strncasecmp(cmd, "dump ", 5) == 0) {
-			last_dump_addr = strtoll(cmd[1]==' '?
-			    cmd + 2 : cmd + 5, NULL, 16);
-			debugger_dump(debugger_emul, last_dump_addr, 8);
-			last_dump_addr += 8*16;
-			/*  Set last cmd to just 'd', so that just pressing
-			    enter will cause dump to continue from the last
-			    address:  */
-			last_cmd_len = 1;
-			strcpy(last_cmd, "d");
+		/*  Special hack for the "step" command:  */
+		if (exit_debugger == -1)
+			return;
 
-		} else if (strcasecmp(cmd, "h") == 0 ||
-		    strcasecmp(cmd, "?") == 0 || strcasecmp(cmd, "help") == 0) {
-			printf("  continue            continues emulation\n");
-			printf("  dump [addr]         dumps emulated memory contents in hex and ASCII\n");
-			printf("  help                prints this help message\n");
+
+#if 0
 			printf("  itrace              toggles instruction_trace on or off (currently %s)\n",
 			    old_instruction_trace? "ON" : "OFF");
-			printf("  quit                quits mips64emul\n");
-			printf("  quiet               toggles quiet_mode on or off (currently %s)\n",
-			    old_quiet_mode? "ON" : "OFF");
-			printf("  registers           dumps all CPUs' register values\n");
-			printf("  step                single steps one instruction\n");
 			printf("  tlbdump             dumps each CPU's TLB contents\n");
 			printf("  trace               toggles show_trace_tree on or off (currently %s)\n",
 			    old_show_trace_tree? "ON" : "OFF");
 			printf("  unassemble [addr]   dumps emulated memory contents as MIPS instructions\n");
-			printf("  version             prints version info\n");
-			last_cmd_len = 0;
 
 		} else if (strcasecmp(cmd, "i") == 0 ||
 		    strcasecmp(cmd, "itrace") == 0) {
@@ -548,14 +583,6 @@ void debugger(void)
 			printf("quiet_mode = %s\n",
 			    old_quiet_mode? "ON" : "OFF");
 
-		} else if (strcasecmp(cmd, "r") == 0 ||
-		    strcasecmp(cmd, "registers") == 0) {
-			for (i=0; i<debugger_emul->ncpus; i++)
-				cpu_register_dump(debugger_emul->cpus[i]);
-			last_cmd_len = 0;
-		} else if (strcasecmp(cmd, "s") == 0 ||
-		    strcasecmp(cmd, "step") == 0) {
-			return;
 		} else if (strcasecmp(cmd, "tl") == 0 ||
 		    strcasecmp(cmd, "tlbdump") == 0) {
 			debugger_tlbdump(debugger_emul);
@@ -582,9 +609,7 @@ void debugger(void)
 			    enter will continue from the last address:  */
 			last_cmd_len = 1;
 			strcpy(last_cmd, "u");
-
 #endif
-
 	}
 
 	debugger_emul->single_step = 0;

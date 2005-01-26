98b78cb6743c97de1e205a1d3fba645aaf4abce0
debug <debug@dbbdf1f2-f3a0-11dc-8807-f0e41a653009>
2005-01-26 10:39:38 +0000

    Beginning the work on the actual parser.
    git-svn-id: file:///home/ryo_on/repos/gxemul-repo/gxemul-legacy/trunk@2368 dbbdf1f2-f3a0-11dc-8807-f0e41a653009

--- src/emul_parse.c
+++ src/emul_parse.c
@@ -25,7 +25,7 @@
  *  SUCH DAMAGE.
  *
  *
- *  $Id: emul_parse.c,v 1.1 2005-01-26 09:26:47 debug Exp $
+ *  $Id: emul_parse.c,v 1.2 2005-01-26 10:39:38 debug Exp $
  *
  *  Set up an emulation by parsing a config file.
  *
@@ -35,9 +35,223 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "emul.h"
 #include "misc.h"
+#include "net.h"
+
+
+#define is_word_char(ch)	( \
+	(ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || \
+	ch == '_' || ch == '$' || (ch >= '0' && ch <= '9') )
+
+
+#define	EXPECT_WORD			1
+#define	EXPECT_LEFT_PARENTHESIS		2
+#define	EXPECT_RIGHT_PARENTHESIS	4
+
+
+/*
+ *  read_one_word():
+ *
+ *  Reads the file f until it has read a complete word. Whitespace is ignored,
+ *  and also any exclamation mark ('!') and anything following an exclamation
+ *  mark on a line.
+ *
+ *  Used internally by emul_parse_config().
+ */
+static void read_one_word(FILE *f, char *buf, int buflen, int *line,
+	int expect)
+{
+	int ch;
+	int done = 0;
+	int curlen = 0;
+	int in_word = 0;
+
+	while (!done) {
+		ch = fgetc(f);
+		if (ch == EOF)
+			break;
+
+		if (in_word) {
+			if (is_word_char(ch)) {
+				buf[curlen++] = ch;
+				if (curlen == buflen - 1)
+					done = 1;
+				continue;
+			} else {
+				if (ungetc(ch, f) == EOF) {
+					fatal("ungetc() failed?\n");
+					exit(1);
+				}
+				break;
+			}
+		}
+
+		if (ch == '\n') {
+			(*line) ++;
+			continue;
+		}
+
+		if (ch == '\r')
+			continue;
+
+		if (ch == '!') {
+			/*  Skip until newline:  */
+			while (ch != '\n' && ch != EOF)
+				ch = fgetc(f);
+			if (ch == '\n')
+				(*line) ++;
+			continue;
+		}
+
+		/*  Whitespace?  */
+		if (ch <= ' ')
+			continue;
+
+		if ((expect & EXPECT_WORD) && is_word_char(ch)) {
+			buf[curlen++] = ch;
+			in_word = 1;
+			if (curlen == buflen - 1)
+				done = 1;
+		} else {
+			if ((expect & EXPECT_LEFT_PARENTHESIS) && ch == '(') {
+				buf[curlen++] = ch;
+				break;
+			}
+			if ((expect & EXPECT_RIGHT_PARENTHESIS) && ch == ')') {
+				buf[curlen++] = ch;
+				break;
+			}
+
+			fatal("unexpected character '%c', line %i\n",
+			    ch, *line);
+			exit(1);
+		}
+	}
+
+	buf[curlen] = '\0';
+}
+
+
+#define	PARSESTATE_NONE		0
+#define	PARSESTATE_EMUL		1
+#define	PARSESTATE_NET		2
+#define	PARSESTATE_MACHINE	3
+
+
+/*
+ *  parse__none():
+ *
+ *  emul ( [...] )
+ */
+static void parse__none(struct emul *e, FILE *f, int *in_emul, int *line,
+	int *parsestate, char *word, size_t maxbuflen)
+{
+	if (strcmp(word, "emul") == 0) {
+		if (*in_emul) {
+			fatal("line %i: only one emul per config "
+			    "file is supported!\n", *line);
+			exit(1);
+		}
+		*parsestate = PARSESTATE_EMUL;
+		*in_emul = 1;
+		read_one_word(f, word, maxbuflen,
+		    line, EXPECT_LEFT_PARENTHESIS);
+		return;
+	}
+
+	fatal("line %i: not expecting '%s' here\n", *line, word);
+	exit(1);
+}
+
+
+/*
+ *  parse__emul():
+ *
+ *  net ( [...] )
+ *  machine ( [...] )
+ */
+static void parse__emul(struct emul *e, FILE *f, int *in_emul, int *line,
+	int *parsestate, char *word, size_t maxbuflen)
+{
+	if (word[0] == ')') {
+		*parsestate = PARSESTATE_NONE;
+		return;
+	}
+
+	if (strcmp(word, "net") == 0) {
+		*parsestate = PARSESTATE_NET;
+		read_one_word(f, word, maxbuflen,
+		    line, EXPECT_LEFT_PARENTHESIS);
+		return;
+	}
+
+	if (strcmp(word, "machine") == 0) {
+		*parsestate = PARSESTATE_MACHINE;
+		read_one_word(f, word, maxbuflen,
+		    line, EXPECT_LEFT_PARENTHESIS);
+		return;
+	}
+
+	fatal("line %i: not expecting '%s' here\n", *line, word);
+	exit(1);
+}
+
+
+/*
+ *  parse__net():
+ *
+ *  TODO: words? for example an option to disable the gateway? that would
+ *  have to be implemented correctly in src/net.c first.
+ */
+static void parse__net(struct emul *e, FILE *f, int *in_emul, int *line,
+	int *parsestate, char *word, size_t maxbuflen)
+{
+	if (word[0] == ')') {
+		/*  Finished with the 'net' section. Let's create the net:  */
+		if (e->net != NULL) {
+			fatal("line %i: more than one net isn't really "
+			    "supported yet\n", *line);
+			exit(1);
+		}
+
+		e->net = net_init(e, NET_INIT_FLAG_GATEWAY, "10.0.0.0", 8);
+		if (e->net == NULL) {
+			fatal("line %i: fatal error: could not create"
+			    " the net (?)\n", *line);
+			exit(1);
+		}
+
+		*parsestate = PARSESTATE_EMUL;
+		return;
+	}
+
+	fatal("line %i: not expecting '%s' here\n", *line, word);
+	exit(1);
+}
+
+
+/*
+ *  parse__machine():
+ *
+ *  TODO: words?
+ */
+static void parse__machine(struct emul *e, FILE *f, int *in_emul, int *line,
+	int *parsestate, char *word, size_t maxbuflen)
+{
+	if (word[0] == ')') {
+		/*  Finished with the 'machine' section.  */
+
+		*parsestate = PARSESTATE_EMUL;
+		return;
+	}
+
+	fatal("line %i: not expecting '%s' here\n", *line, word);
+	exit(1);
+}
 
 
 /*
@@ -47,6 +261,54 @@
  */
 void emul_parse_config(struct emul *e, FILE *f)
 {
-	debug("emul_parse_config()\n");
+	char word[500];
+	int in_emul = 0;
+	int line = 1;
+	int parsestate = PARSESTATE_NONE;
+
+	/*  debug("emul_parse_config()\n");  */
+
+	while (!feof(f)) {
+		read_one_word(f, word, sizeof(word), &line,
+		    EXPECT_WORD | EXPECT_RIGHT_PARENTHESIS);
+		if (!word[0])
+			break;
+
+		debug("word = '%s'\n", word);
+
+		switch (parsestate) {
+
+		case PARSESTATE_NONE:
+			parse__none(e, f, &in_emul, &line, &parsestate,
+			    word, sizeof(word));
+			break;
+
+		case PARSESTATE_EMUL:
+			parse__emul(e, f, &in_emul, &line, &parsestate,
+			    word, sizeof(word));
+			break;
+
+		case PARSESTATE_NET:
+			parse__net(e, f, &in_emul, &line, &parsestate,
+			    word, sizeof(word));
+			break;
+
+		case PARSESTATE_MACHINE:
+			parse__machine(e, f, &in_emul, &line, &parsestate,
+			    word, sizeof(word));
+			break;
+
+		default:
+			fatal("INTERNAL ERROR in emul_parse.c ("
+			    "parsestate %i is not imlemented yet?)\n",
+			    parsestate);
+			exit(1);
+		}
+	}
+
+	if (parsestate != PARSESTATE_NONE) {
+		fatal("EOF but not enough right parentheses?\n");
+		exit(1);
+	}
 }
 

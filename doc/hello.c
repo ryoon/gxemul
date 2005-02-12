c933ecae9db62ff33b7252f1b5e9cdf5173a5016
debug <debug@dbbdf1f2-f3a0-11dc-8807-f0e41a653009>
2005-02-12 10:05:09 +0000

    Update.
    git-svn-id: file:///home/ryo_on/repos/gxemul-repo/gxemul-legacy/trunk@2544 dbbdf1f2-f3a0-11dc-8807-f0e41a653009

--- doc/hello.c
+++ doc/hello.c
@@ -1,9 +1,9 @@
 /*  Hello world for mips64emul  */
 
-/*  Note: The cast to an int causes the address to be sign-extended
+/*  Note: The cast to a signed int causes the address to be sign-extended
     correctly to 0xffffffffb00000xx when compiled in 64-bit mode  */
-#define	PUTCHAR_ADDRESS		((int)0xb0000000)
-#define	HALT_ADDRESS		((int)0xb0000010)
+#define	PUTCHAR_ADDRESS		((signed int)0xb0000000)
+#define	HALT_ADDRESS		((signed int)0xb0000010)
 
 void printchar(char ch)
 {

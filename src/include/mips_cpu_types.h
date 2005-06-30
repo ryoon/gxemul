cc2728ddea2f8f5dcacad33df58b54191fba3aad
debug <debug@dbbdf1f2-f3a0-11dc-8807-f0e41a653009>
2005-06-30 15:36:53 +0000

    Update.
    git-svn-id: file:///home/ryo_on/repos/gxemul-repo/gxemul-legacy/trunk@3194 dbbdf1f2-f3a0-11dc-8807-f0e41a653009

--- src/include/mips_cpu_types.h
+++ src/include/mips_cpu_types.h
@@ -28,7 +28,7 @@
  *  SUCH DAMAGE.
  *
  *
- *  $Id: mips_cpu_types.h,v 1.7 2005-06-30 10:52:51 debug Exp $
+ *  $Id: mips_cpu_types.h,v 1.8 2005-06-30 15:36:53 debug Exp $
  *
  *  MIPS CPU types.
  */
@@ -100,7 +100,7 @@
 	{ "RM7900",	0 /*TODO*/, 0x0 /* ? */,DCOUNT,	EXC4K, MMU4K,	4,	64, 4,14,5,1,14,5,1,18, 6, 1 }, /*  instrs/cycle? cachelinesize? assoc = 4ways for all  */ \
 	{ "RM9000",	0x34,       0x0 /* ? */,DCOUNT,	EXC4K, MMU4K,	4,	48, 4,14,5,1,14,5,1,18, 6, 1 }, /*  This is totally bogus  */ \
 	{ "RC32334",	MIPS_RC32300,0x00,	0,	EXC32, MMU4K,  32,      16, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, \
-	{ "4Kc",	0x100+MIPS_4Kc, 1,	0,	EXC32, MMU32,  32,	16, 4,14,5,2,14,5,2, 0, 0, 0 }, /*  DCOUNT?  instrs/cycle? BOGUS, TODO  */ \
+	{ "4Kc",	0x100+MIPS_4Kc, 1,	0,	EXC32, MMU32,  32,	16, 4,14,4,2,14,4,2, 0, 0, 0 }, /*  DCOUNT?  instrs/cycle? BOGUS, TODO  */ \
 	{ "5Kc",	0x100+MIPS_5Kc, 1,	0,	EXC64, MMU64,  64,	48, 4,15,5,2,15,5,2, 0, 0, 0 }, /*  DCOUNT?  instrs/cycle? BOGUS, TODO  */ \
 	{ "BCM4710",	0x000240,   0x00,       0,	EXC32, MMU32,  32,      32, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, /*  TODO: this is just bogus  */ \
 	{ "BCM4712",	0x000290,   0x07,       0,	EXC32, MMU32,  32,      32, 2,13,4,1,12,4,1, 0, 0, 0 }, /*  2ways I, 2ways D  */ \
@@ -109,7 +109,7 @@
 	{ "AU1100",	0x020301,   0x00,       0,	EXC32, MMU32,  32,      32, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, /*  TODO: this is just bogus  */ \
 	{ "SB1",	0x000401,   0x00,	0,	EXC64, MMU64,  64,      32, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, /*  TODO: this is just bogus  */ \
 	{ "SR7100",	0x000504,   0x00,	0,	EXC64, MMU64,  64,      32, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, /*  TODO: this is just bogus  */ \
-	{ "Allegrex",	0x000000,   0x00,	0,	EXC3K, MMU32,   2,       4, 1,14,6,2,14,6,2, 0, 0, 0 }, \
+	{ "Allegrex",	0x000000,   0x00,	0,	EXC32, MMU32,   2,       4, 1,14,6,2,14,6,2, 0, 0, 0 }, \
 	{ NULL,		0,          0,          0,      0,     0,       0,       0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 } }
 
 #endif	/*  CPU_TYPES_H  */

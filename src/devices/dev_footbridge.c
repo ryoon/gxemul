09a5f8eea97548fb4de3a0da7fca3530f9cdba5c
debug <debug@dbbdf1f2-f3a0-11dc-8807-f0e41a653009>
2005-09-10 07:04:26 +0000

    Update.
    git-svn-id: file:///home/ryo_on/repos/gxemul-repo/gxemul-legacy/trunk@3524 dbbdf1f2-f3a0-11dc-8807-f0e41a653009

--- src/devices/dev_footbridge.c
+++ src/devices/dev_footbridge.c
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2004-2005  Anders Gavare.  All rights reserved.
+ *  Copyright (C) 2005  Anders Gavare.  All rights reserved.
  *
  *  Redistribution and use in source and binary forms, with or without
  *  modification, are permitted provided that the following conditions are met:
@@ -23,11 +23,11 @@
  *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
  *  OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  *  SUCH DAMAGE.
- *   
  *
- *  $Id: dev_footbridge.c,v 1.1 2005-09-10 00:20:07 debug Exp $
- *  
- *  Footbridge. Bus used in Netwinder.
+ *
+ *  $Id: dev_footbridge.c,v 1.2 2005-09-10 07:00:12 debug Exp $
+ *
+ *  Footbridge. Used in Netwinder and Cats.
  *
  *  TODO.
  */

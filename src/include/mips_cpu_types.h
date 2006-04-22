cd9b62229db00dfffe70da8376ad278084b8c322
debug <debug@dbbdf1f2-f3a0-11dc-8807-f0e41a653009>
2006-04-22 14:33:09 +0000

    *** empty log message ***
    git-svn-id: file:///home/ryo_on/repos/gxemul-repo/gxemul-legacy/trunk@4048 dbbdf1f2-f3a0-11dc-8807-f0e41a653009

--- src/include/mips_cpu_types.h
+++ src/include/mips_cpu_types.h
@@ -2,7 +2,7 @@
 #define	MIPS_CPU_TYPES_H
 
 /*
- *  Copyright (C) 2003-2005  Anders Gavare.  All rights reserved.
+ *  Copyright (C) 2003-2006  Anders Gavare.  All rights reserved.
  *
  *  Redistribution and use in source and binary forms, with or without
  *  modification, are permitted provided that the following conditions are met:
@@ -28,7 +28,7 @@
  *  SUCH DAMAGE.
  *
  *
- *  $Id: mips_cpu_types.h,v 1.13 2006-01-11 19:20:08 debug Exp $
+ *  $Id: mips_cpu_types.h,v 1.14 2006-04-22 14:33:09 debug Exp $
  *
  *  MIPS CPU types.
  */

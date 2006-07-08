ab7f4ed0fb9385d6dc8e981a759abd9e4c9210bd
debug <debug@dbbdf1f2-f3a0-11dc-8807-f0e41a653009>
2006-07-08 22:38:42 +0000

    *** empty log message ***
    git-svn-id: file:///home/ryo_on/repos/gxemul-repo/gxemul-legacy/trunk@4270 dbbdf1f2-f3a0-11dc-8807-f0e41a653009

--- experiments/test_netbsd_pmax_install.sh
+++ experiments/test_netbsd_pmax_install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-#  $Id: test_netbsd_pmax_install.sh,v 1.1 2006-07-08 21:49:08 debug Exp $
+#  $Id: test_netbsd_pmax_install.sh,v 1.2 2006-07-08 22:38:42 debug Exp $
 #
 #  Litet enkelt test för att mäta hur lång tid det tar att installera
 #  en full NetBSD/pmax 3.0 för R3000, utan interaktion.
@@ -11,5 +11,12 @@
 
 rm -f nbsd_pmax.img
 dd if=/dev/zero of=nbsd_pmax.img bs=1024 count=1 seek=1900000
-time experiments/test_netbsd_pmax_install.expect 
+sync
+sleep 2
 
+time experiments/test_netbsd_pmax_install.expect 2> /tmp/gxemul_result
+
+echo
+echo
+echo
+cat /tmp/gxemul_result

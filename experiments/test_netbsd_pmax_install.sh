d4f8202b6b4f4c9128d849ec7212785ed3769b62
debug <debug@dbbdf1f2-f3a0-11dc-8807-f0e41a653009>
2006-07-08 21:49:08 +0000

    Adding automated performance tests using Expect.
    git-svn-id: file:///home/ryo_on/repos/gxemul-repo/gxemul-legacy/trunk@4269 dbbdf1f2-f3a0-11dc-8807-f0e41a653009

---
+++ experiments/test_netbsd_pmax_install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#
+#  $Id: test_netbsd_pmax_install.sh,v 1.1 2006-07-08 21:49:08 debug Exp $
+#
+#  Litet enkelt test för att mäta hur lång tid det tar att installera
+#  en full NetBSD/pmax 3.0 för R3000, utan interaktion.
+#  Starta med:
+#
+#	experiments/test_netbsd_pmax_install.sh
+#
+
+rm -f nbsd_pmax.img
+dd if=/dev/zero of=nbsd_pmax.img bs=1024 count=1 seek=1900000
+time experiments/test_netbsd_pmax_install.expect 
+

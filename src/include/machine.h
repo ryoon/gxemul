bcaac1bba26c52448cfbd9673c26ff368123b22d
debug <debug@dbbdf1f2-f3a0-11dc-8807-f0e41a653009>
2007-05-31 16:53:46 +0000

    Removing the mmEye machine mode skeleton.
    git-svn-id: file:///home/ryo_on/repos/gxemul-repo/gxemul-legacy/trunk@4889 dbbdf1f2-f3a0-11dc-8807-f0e41a653009

--- src/include/machine.h
+++ src/include/machine.h
@@ -28,7 +28,7 @@
  *  SUCH DAMAGE.
  *
  *
- *  $Id: machine.h,v 1.169 2007-05-26 22:26:31 debug Exp $
+ *  $Id: machine.h,v 1.170 2007-05-31 16:53:46 debug Exp $
  */
 
 #include <sys/types.h>
@@ -276,7 +276,6 @@ struct machine {
 #define	MACHINE_HPCSH		6002
 #define	MACHINE_DREAMCAST	6003
 #define	MACHINE_LANDISK		6004
-#define	MACHINE_MMEYE		6005
 
 /*  M88K:  */
 #define	MACHINE_BAREM88K	7000

a1f8a7d0924efa405dbed9a20a53e838a4d508d1
Ryo ONODERA <ryo@tetera.org>
2019-07-29 07:27:22 +0900

    Apply pkgsrc patch
    sd0 -> rz0 so we can find our boot device.

--- src/machines/machine_pmax.cc
+++ src/machines/machine_pmax.cc
@@ -804,7 +804,7 @@ abort();
 			strlcpy(bootpath, "rz(0,0,0)", sizeof(bootpath));
 		else
 #endif
-			strlcpy(bootpath, "5/rz1/", sizeof(bootpath));
+			strlcpy(bootpath, "5/rz0/", sizeof(bootpath));
 
 		if (machine->bootdev_id < 0 || machine->force_netboot) {
 			/*  tftp boot:  */
